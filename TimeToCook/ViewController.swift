8cc119784f4381382fb72a6bdf57ac74ebe06ec1
Nikita Gvozdikov <pounddnuop@mail.ru>
2021-07-20 15:53:13 +0300

    Initial Commit

---
+++ TimeToCook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeToCook
+//
+//  Created by Никита Гвоздиков on 20.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
