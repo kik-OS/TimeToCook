d5bb955484442e98d7a86db23d2309378d1043e8
Nikita Gvozdikov <pounddnuop@mail.ru>
2021-08-19 09:19:42 +0300

    PI VM Tsts

--- TimeToCookTests/TimeToCookTests.swift
+++ TimeToCookTests/TimeToCookTests.swift
@@ -11,17 +11,17 @@ import XCTest
 var sut: ProductInfoViewModel?
 
 class ProductInfoViewModelTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
         sut = ProductInfoViewModel()
     }
-
+    
     override func tearDownWithError() throws {
         sut = nil
         try super.tearDownWithError()
     }
-
+    
     func testThatInitializationOfProductIsWorking() {
         // arrange
         let product = Product(code: "", title: "", producer: "",
@@ -49,4 +49,47 @@ class ProductInfoViewModelTests: XCTestCase {
         // assert
         XCTAssertFalse(productInfoViewModel.buttonStartCookTapped)
     }
+    
+    func testThatPropertyProductImageReturnCorrectImageName() {
+        // arrange
+        let product = Product(code: "", title: "", producer: "",
+                              category: "Рис", weight: 0,
+                              cookingTime: 0, intoBoilingWater: nil,
+                              needStirring: nil, waterRatio: 0)
+        
+        // act
+        let productInfoViewModel = ProductInfoViewModel(product: product)
+        
+        // assert
+        XCTAssertEqual(productInfoViewModel.productImage, "Рис.png")
+    }
+    
+    func testThatPropertyWeightReturnCorrectProductWeight() {
+        // arrange
+        let product = Product(code: "", title: "", producer: "",
+                              category: "", weight: 200,
+                              cookingTime: 0, intoBoilingWater: nil,
+                              needStirring: nil, waterRatio: 0)
+        
+        // act
+        let productInfoViewModel = ProductInfoViewModel(product: product)
+        
+        // assert
+        XCTAssertEqual(productInfoViewModel.weight, "200 г.")
+    }
+    
+    func testThatPropertyCookingTimeReturnCorrectTimeOfCooking() {
+        // arrange
+        let product = Product(code: "", title: "", producer: "",
+                              category: "", weight: 0,
+                              cookingTime: 20, intoBoilingWater: nil,
+                              needStirring: nil, waterRatio: 0)
+        
+        // act
+        let productInfoViewModel = ProductInfoViewModel(product: product)
+        
+        // assert
+        XCTAssertEqual(productInfoViewModel.cookingTime, "20 мин.")
+    }
+    
 }
