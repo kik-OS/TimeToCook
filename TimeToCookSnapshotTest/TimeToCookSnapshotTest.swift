474558bad68c044d8c129c0f320530a84875b791
Nikita Gvozdikov <pounddnuop@mail.ru>
2021-08-31 16:19:18 +0300

    Snapshot

--- TimeToCookSnapshotTest/TimeToCookSnapshotTest.swift
+++ TimeToCookSnapshotTest/TimeToCookSnapshotTest.swift
@@ -11,12 +11,30 @@ import SnapshotTesting
 
 class TimeToCookSnapshotTest: XCTestCase {
 
-    var sut: ProductInfoViewController?
+    var sut: TimerViewController?
+    var viewModel: TimerViewModel?
+    var timerServise: TimerServiceProtocol?
+    var notificationService: NotificationServiceProtocol?
 
-    override func setUp() {
-        sut = ProductInfoViewController(viewModel: ProductInfoViewModel(timerService: TimerService(), notificationService: NotificationService()))
-      }
-    func test_screenSnapshot() {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+
+        timerServise = TimerService()
+        notificationService = NotificationService()
+        viewModel = TimerViewModel(timerService: timerServise!,
+                                   notificationService: notificationService!)
+        sut = TimerViewController(viewModel: viewModel!)
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        timerServise = nil
+        notificationService = nil
+        viewModel = nil
+        try super.tearDownWithError()
+    }
+
+    func testScreenSnapshot() {
         assertSnapshot(matching: sut!, as: .image(on: .iPhoneSe))
-      }
+    }
 }
